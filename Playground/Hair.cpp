55ec95f023c950990020f2ec473f9e9eb70a3f6d
enocho1 <13eomale@mtsn.org.uk>
2023-04-28 12:23:29 -0400

    think i've got parallel transport, but i'll need to visualize it before i know it works

--- Playground/Hair.cpp
+++ Playground/Hair.cpp
@@ -27,7 +27,7 @@ void Hair::smooth(const dVector& x, double alpha)
 		int prev = 3 * particles[i - 1];
 		auto p1 = P3D(x[current], x[current + 1], x[current + 2]);
 		auto p2 = P3D(x[prev], x[prev + 1], x[prev + 2]);
-		
+
 		V3D d = 1.0 * (1.0 - beta) * d_1 - (1.0 - beta) * (1.0 - beta) * d_2 + beta * beta * (p2 - p1);
 
 		auto next = d + smoothed[smoothed.size() - 1];
@@ -57,6 +57,58 @@ void Hair::initializeFrames(const dVector& x)
 
 void Hair::updateFrames(const dVector& x)
 {
+	//smooth
+	smooth(x, 10);
+	V3D t, t_1;
+	vector<V3D>normals;
+	
+	//first normal:
+	V3D first_tangent = (smoothed[1] - smoothed[0]).normalized();
+	auto first_normal = V3D(0, 1, 0);
+	V3D helper = (first_normal.cross(first_tangent)).normalized();
+	first_normal = (first_tangent.cross(helper)).normalized();
+	normals.push_back(first_normal);
+	double theta = 0;
+	V3D bitangent;
+
+	Matrix3x3 frame;
+
+	
+
+
+	//parallel transport over smoothed curve
+	for (int i = 0; i < particles.size(); i++) {
+		if (i + 1 < particles.size()) {
+			t = smoothed[i + 1] - smoothed[i];
+		}
+		else {
+			t = smoothed[i] - smoothed[i - 1];
+		}
+		if (i + 2 < particles.size()) {
+			t_1 = smoothed[i + 2] - smoothed[i + 1];
+		}
+		else {
+			t_1 = t;
+		}
+		bitangent = t.cross(t_1);
+
+		if (t.length() == 0) {
+			normals.push_back(normals[normals.size() - 1]);
+		}
+		else {
+			bitangent.normalize();
+			theta = acos(t.dot(t_1));
+			normals.push_back(rotateVec(normals[normals.size() - 1], theta, bitangent));
+		}
+		bitangent = t.cross(normals[i]);
+		frame.col(0) = t;
+		frame.col(2) = bitangent;
+		frame.col(3) = normals[i];
+
+		
+
+	}
+
 }
 
 void Hair::integrateForces(const dVector& x, const dVector& v, dVector& f)
